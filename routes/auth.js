e67434e03ff3f8c253a997eabdcd86f29090993f
Juan Cabello <jcabelloc@itana.pe>
2024-09-20 19:33:42 -0500

    Otras validaciones y validaciones personalizadas

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 
 const authController = require('../controllers/auth');
@@ -12,9 +12,31 @@ router.post('/ingresar', authController.postIngresar);
 
 router.get('/registrarse', authController.getRegistrarse);
 
-router.post('/registrarse', check('email')
-    .isEmail()
-    .withMessage('Por favor ingrese un email valido'), authController.postRegistrarse);
+router.post('/registrarse', 
+    [
+        check('email')
+          .isEmail()
+          .withMessage('Por favor ingrese un email valido')
+          .custom((value, { req }) => {
+            if (value === 'no-reply@gmail.com') {
+              throw new Error('Este email no permitido');
+            }
+            return true;
+          }),
+        body(
+          'password',
+          'Por favor ingrese un password que tenga solo letras o números y no menos de 5 caracteres.'
+        )
+          .isLength({ min: 5 })
+          .isAlphanumeric(),
+        body('passwordConfirmado').custom((value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Passwords no coinciden!');
+          }
+          return true;
+        })
+    ],
+    authController.postRegistrarse);
 
 router.post('/salir', authController.postSalir);
 
